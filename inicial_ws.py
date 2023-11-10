9495442ccf39d7b4d300a168a030e5176f4f1406
Bianca Portes <bianca.portes.c@gmail.com>
2023-11-10 11:11:48 -0300

    Create inicial_ws.py
    Adição do arquivo que usa requests

---
+++ inicial_ws.py
@@ -0,0 +1,14 @@
+"""
+Original file is located at
+    https://colab.research.google.com/drive/1o2f8Eh13GaZcnO4ULq>
+
+#Web Scraping
+
+É a técnica de extrair dados de websites. Nessa situação o seu >
+
+O web scraping é permitido quando realizado em **dados públicos>
+
+## Primeiros passos
+
+Para começar, vamos pegar o conteúdo HTML de uma página Web.
+"""
